1d3f4b7a445c56ff54b8b785dfa126d9b3125b97
Minhthang1506 <19522216@gm.uit.edu.vn>
2021-05-07 13:53:33 +0700

    thang_init

--- Planzy/obj/Debug/net5.0-windows/Views/Button_Function_View.g.i.cs
+++ Planzy/obj/Debug/net5.0-windows/Views/Button_Function_View.g.i.cs
@@ -54,7 +54,7 @@ namespace Planzy.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Planzy;component/views/button_function_view.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Planzy;V1.0.0.0;component/views/button_function_view.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\Button_Function_View.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
