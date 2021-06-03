eb82c09df1dfcfad6c313a1aa7410ca5a7d9b7ce
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2021-06-03 14:31:00 +0700

    update

---
+++ Planzy/Models/SupportUI/ButtonDuocChon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+namespace Planzy.Models.SupportUI
+{
+    class ButtonDuocChon : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string newForeground;
+
+        public string NewForeground
+        {
+            get { return newForeground; }
+            set { newForeground = value; OnPropertyChanged("NewForeground"); }
+        }
+        private string newBackground;
+
+        public string NewBackground
+        {
+            get { return newBackground; }
+            set { newBackground = value; OnPropertyChanged("NewBackground"); }
+        }
+
+        private string newVisibility;
+
+        public string NewVisibility
+        {
+            get { return newVisibility; }
+            set { newVisibility = value; OnPropertyChanged("NewVisibility"); }
+        }
+        public ButtonDuocChon(bool isDuocChon)
+        {
+            if (isDuocChon)
+            {
+                newBackground = "#FF5867AC";
+                newForeground = "#ffffffff";
+                newVisibility = "Visible";
+            }    
+            else
+            {
+                newBackground = "#ffffffff";
+                newForeground = "#FF5867AC";
+                newVisibility = "Hidden";
+            }    
+        }
+    }
+}
