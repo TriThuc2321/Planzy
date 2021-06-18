9899ceb3a7b32017c4a39e8dbcaa2404421a922b
Minhthang1506 <19522216@gm.uit.edu.vn>
2021-06-18 14:05:37 +0700

    fix ảnh mesbox

--- Planzy/Views/CustomMessageBoxWindow.xaml.cs
+++ Planzy/Views/CustomMessageBoxWindow.xaml.cs
@@ -194,10 +194,10 @@ namespace FootballFieldManagement.Views
             switch (image)
             {
                 case MessageBoxImage.Warning:
-                    bitmapImage = new BitmapImage(new Uri("LoiImage.png", UriKind.Relative));
+                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/Planzy;component/Resources/Images/CanhBaoimage.png"));
                     break;
                 case MessageBoxImage.Asterisk:
-                    bitmapImage = new BitmapImage(new Uri("HoanTatImage.png", UriKind.Relative));
+                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/Planzy;component/Resources/Images/HoanTatImage.png"));
                     break;
                 //case MessageBoxImage.Error:
                 //    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/FootballFieldManagement;component/Resources/Images/error.png"));
@@ -206,7 +206,7 @@ namespace FootballFieldManagement.Views
                 //    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/FootballFieldManagement;component/Resources/Images/help.png"));
                 //    break;
                 default:
-                    bitmapImage = new BitmapImage(new Uri("LoiImage.png",UriKind.Relative));
+                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/Planzy;component/Resources/Images/LoiImage.png"));
                     break;
             }
 
