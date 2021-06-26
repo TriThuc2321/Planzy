73b35cf48aaff2aa8860c40de44294f4d0f16917
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2021-06-27 03:04:13 +0700

    1

--- Planzy/obj/Debug/net5.0-windows/Views/InsertTicketDialog.g.i.cs
+++ Planzy/obj/Debug/net5.0-windows/Views/InsertTicketDialog.g.i.cs
@@ -9,9 +9,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using MaterialDesignThemes.Wpf;
-using MaterialDesignThemes.Wpf.Converters;
-using MaterialDesignThemes.Wpf.Transitions;
 using Planzy.ViewModels;
 using Planzy.Views;
 using System;
@@ -65,7 +62,7 @@ namespace Planzy.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Planzy;component/views/insertticketdialog.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Planzy;V1.0.0.0;component/views/insertticketdialog.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\InsertTicketDialog.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
